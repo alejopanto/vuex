ce07620955844fa72bd7fd9c7723cb8eeeb1bb5f
alejopanto <jose.pantoja@testu.co>
2023-03-29 17:08:06 -0500

    mapState

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,11 @@
 import { createStore } from 'vuex'
 
 const store = createStore({
-  
+    state() {
+        return {
+            username: 'Jose Pantoja 3'
+        }
+    }
 })
 
 export default store
